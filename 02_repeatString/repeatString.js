8d72ed0c0ba7173a15e2a95307aebb2a2821e62f
elitecdog <elitecdog@gmail.com>
2022-05-21 17:07:55 -0500

    Created a function that repeats a given string a given number of times that passes all tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num<0) {
+        return 'ERROR';
+    }
+    let stringFinal = "";
+    for(let i=0; i<num; i++) {
+        stringFinal+= string;
+    }
+    return stringFinal;
 };
 
 // Do not edit below this line
