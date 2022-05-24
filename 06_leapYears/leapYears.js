e5860ba46a5ed436b474deebd12b69ef4944c16d
elitecdog <elitecdog@gmail.com>
2022-05-23 20:14:42 -0500

    Created a function that tests for leap years.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let centuryTest = year%100;
+    let test400 = year%400;
+    let normalTest = year%4;
+    if(test400 == 0) {
+        return true;
+    }
+    else if(centuryTest == 0) {
+        return false;
+    }
+    else if(normalTest == 0) {
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
