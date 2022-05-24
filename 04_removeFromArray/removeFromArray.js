d89448b54ba89e4db456f5e118c64e1ce91a18fb
elitecdog <elitecdog@gmail.com>
2022-05-23 19:40:46 -0500

    Created a remove from array function that will remove any argument from an array that has matching value and type.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let arr2 = arr;
+    for(let i=0; i<arr2.length; i++) {
+        for(let j=1; j<arguments.length; j++){
+        if(arr2[i] === arguments[j]) {
+            arr2.splice(i, 1);
+            i--;
+        }
+    }
+}
+    return arr2;
 };
 
 // Do not edit below this line
