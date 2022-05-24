d3fce592a3d3071de6e809302b6ad1c9d848245d
elitecdog <elitecdog@gmail.com>
2022-05-23 20:06:05 -0500

    Created a function that somes all numbers between two given numbers.  This function will return error for any negative or non integer parameter.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(int, int2) {
+    let sum=0;
+    if(typeof int !=="number" || typeof int2 !=="number") {
+        return "ERROR";
+    }
+    else if(int<0 || int2 <0) {
+        return "ERROR";
+    }
+    else if(int<int2) {
+    for(let i=int; i<=int2; i++) {
+        sum+=i;
+    }
+    return sum;
+}
+    else if(int>int2) {
+        for(let i=int2; i<=int; i++) {
+            sum+=i;
+    }
+        return sum;
+}
+    else {
+        let sum= int;
+        return sum;
+    }
 };
 
 // Do not edit below this line
