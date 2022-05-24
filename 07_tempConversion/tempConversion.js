14ec8bfac223fd08f1fd59d37dfee33531856176
elitecdog <elitecdog@gmail.com>
2022-05-23 20:28:42 -0500

    Created two temp conversion functions to go from f --> c and c --> f

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  let multiplier = 5/9;
+  let preMulti = input-32;
+  let celcius = preMulti * multiplier;
+  let output = Math.round(celcius*10)/10;
+  return output;
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let multiplier = 9/5;
+  let postMulti = input * multiplier;
+  let preFinal = postMulti +32;
+  let output = Math.round(preFinal*10)/10;
+  return output;
 };
 
 // Do not edit below this line
