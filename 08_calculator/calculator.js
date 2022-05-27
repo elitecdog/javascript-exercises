beef0ae87d2dc78d88fd7ab6e15d9622c9ac4934
elitecdog <elitecdog@gmail.com>
2022-05-27 14:31:55 -0500

    Created all necessary functions to make the calculator work

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total=0;
+  for(let i=0; i<arr.length; i++){
+    total+= arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total= 1;
+  for(let i=0; i<arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return Math.pow(base,power);
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	let total = 1;
+  let counter = input;
+  for(let i=0; i<input; i++) {
+    total*=counter;
+    counter--; 
+  }
+  return total;
 };
 
 // Do not edit below this line
