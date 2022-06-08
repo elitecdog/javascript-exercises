b7b1d7b6640be41725b0507a258db43d7ebe6178
elitecdog <elitecdog@gmail.com>
2022-06-08 14:45:00 -0500

    Completed activity

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,35 @@
-const fibonacci = function() {
 
+function fibonacci(input) {
+    if(input<0) {
+        return "OOPS";
+    }
+    let arr = [1,1];
+    let fib1=1;
+    let fib2=1;
+    for(let i =2; i<input; i++) {
+        arr.push(fib1+fib2);
+        let temp = fib1;
+        fib1= fib2+fib1;
+        fib2=temp;
+    }
+    return arr[input-1];
+    /*if (input==1 || input ==2) {
+        return 1;
+    }
+    else if(input<0) {
+        return "OOPS";
+    }
+    let prev = 1;
+    let prev2 = 1;
+    let temp=0;
+    for(let i=2; i<input; i++) {
+        let temp = prev;
+        let prev = prev + prev2;
+        let prev2 = temp;
+    }
+    return prev;
+
+    */
 };
 
 // Do not edit below this line
